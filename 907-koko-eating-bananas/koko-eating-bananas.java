ca8a5587df997add75b86da3fbd73549984c96fb
Akul Tyagi <166997576+Akul-Tyagi@users.noreply.github.com>
2026-03-14 19:19:27 +0530

    Time: 12 ms (55.46%) | Memory: 47.9 MB (54.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,7 +1,10 @@
 public class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;  
-        int right = Arrays.stream(piles).max().getAsInt();
+        int right = 0;
+        for(int i=0; i<piles.length;i++){
+            right = Math.max(right, piles[i]);
+        }
         int ans = right;
 
         while (left <= right) {
