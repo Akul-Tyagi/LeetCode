394fa012f7a953ad4fb68a80ff055883a528dd6a
Akul Tyagi <akultyagi.cs@gmail.com>
2026-05-30 13:50:27 +0530

    Time: 5 ms (84.67%) | Memory: 46.6 MB (89.96%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,42 +1,37 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // Topological sort
-        // Edge case
-        if(numCourses <= 0) return new int[0];
-        
-        //1. Init Map
-        HashMap<Integer, Integer> inDegree = new HashMap<>();
-        HashMap<Integer, List<Integer>> topoMap = new HashMap<>();
-        for(int i = 0; i < numCourses; i++) {
-            inDegree.put(i, 0);
-            topoMap.put(i, new ArrayList<Integer>());
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
         }
-        
-        //2. Build Map
-        for(int[] pair : prerequisites) {
-            int curCourse = pair[0], preCourse = pair[1];
-            topoMap.get(preCourse).add(curCourse);  // put the child into it's parent's list
-            inDegree.put(curCourse, inDegree.get(curCourse) + 1); // increase child inDegree by 1
+
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int prereq = edge[1];
+            graph.get(prereq).add(course);
+            inDegree[course]++;
         }
-        //3. find course with 0 indegree, minus one to its children's indegree, until all indegree is 0
-        int[] res = new int[numCourses];
-        int base = 0;
-        while(!inDegree.isEmpty()) {
-            boolean flag = false;   // use to check whether there is cycle
-            for(int key : inDegree.keySet()) {  // find nodes with 0 indegree
-                if(inDegree.get(key) == 0) {
-                    res[base ++] = key;
-                    List<Integer> children = topoMap.get(key);  // get the node's children, and minus their inDegree
-                    for(int child : children) 
-                        inDegree.put(child, inDegree.get(child) - 1);
-                    inDegree.remove(key);      // remove the current node with 0 degree and start over
-                    flag = true;
-                    break;
-                }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) queue.offer(i);
+        }
+
+        int[] order = new int[numCourses];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            order[index++] = curr;
+
+            for (int neighbor : graph.get(curr)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) queue.offer(neighbor);
             }
-            if(!flag)  // there is a circle --> All Indegree are not 0
-                return new int[0];
         }
-        return res;
+
+        return index == numCourses ? order : new int[0];
     }
 }
